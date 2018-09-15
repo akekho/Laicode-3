27ac02e158b6992c24c3ce02960dd8197232823e
tonglyu <tonglyu9647@gmail.com>
2018-09-14 17:18:24 -0700

    all valid parenthesis I

---
+++ Class7_DFS/AllValidPermutationsOfParenthesesI.java
@@ -0,0 +1,59 @@
+/**
+ * Description
+ * Given N pairs of parentheses “()”, return a list with all the valid permutations.
+ * 
+ * Assumptions
+ * N >= 0
+ * 
+ * Examples
+ * N = 1, all valid permutations are ["()"]
+ * N = 3, all valid permutations are ["((()))", "(()())", "(())()", "()(())", "()()()"]
+ * N = 0, all valid permutations are [""]
+ * 
+ * Time = O(2^(2n))
+ * Space = O(2n)
+ */
+
+public class Solution {
+    public List<String> validParenthesis(int k) {
+        List<String> result = new ArrayList<>();
+        // the final string contains 2k characters.
+        char[] cur = new char[k * 2];
+        DFS(cur, k, k, 0, result);
+        return result;
+    }
+
+    // left: how many ‘(’ we still have
+    // right: how many ‘)’ we still have
+    // index: the current position in cur we want to fill in with either ‘(’ or ‘)’
+    public void DFS(char[] cur, int left, int right, int index, List<String> result) {
+        // terminate condition:
+        // when we do not have any parenthesis left
+        if (left == 0 && right == 0) {
+            result.add(new String(cur));
+            return;
+        }
+        // when can we add a ‘(‘? whenever there is some ‘(‘ we can still use.
+        if (left > 0) {
+            cur[index] = '(';
+            DFS(cur, left - 1, right, index + 1, result);
+            // NOTICE: it looks like we donot do anything when back tracking to
+            // the previous level.
+            // the code is still correct because:
+            // 1. we are setting the character at index and when back tracking,
+            // what we need is just 1) remove the character at index and 2) add
+            // a different character at index.
+            // 2. only when we fill in all the position in cur, we have a complete
+            // solution.
+            // thec code itself actually already suffices the above two points and
+            // it already does the currect removing operation when back tracked to //the
+            // previous level.
+        }
+        // when can we add a ‘)’? when there is more ‘(‘ than ‘)’ used.
+        // because each ‘)’ should be associated with a previous ‘(‘
+        if (right > left) {
+            cur[index] = ')';
+            DFS(cur, left, right - 1, index + 1, result);
+        }
+    }
+}
--- template.java
+++ template.java
@@ -1,50 +1,43 @@
 public class Solution {
-    public int kthSmallest(int[][] matrix, int k) {
-        int rows = matrix.length;
-        int columns = matrix[0].length;
-        // Best First Search, need a minheap on the value of each cells.
-        PriorityQueue<Cell> minHeap = new PriorityQueue<Cell>(k, new Comparator<Cell>() {
-            @Override
-            public int compare(Cell c1, Cell c2) {
-                if (c1.value == c2.value) {
-                    return 0;
-                }
-                return c1.value < c2.value ? -1 : 1;
-            }
-        });
-        // all the generated cells will be marked true,
-        // to avoid being generated more than once.
-        boolean[][] visited = new boolean[rows][columns];
-        minHeap.offer(new Cell(0, 0, matrix[0][0]));
-        visited[0][0] = true;
-        // iterate k-1 rounds, and BFS the smallest k-1 cells.
-        for (int i = 0; i < k - 1; i++) {
-            Cell cur = minHeap.poll();
-            // the neighbor cell will be inserted back to the minheap only if
-            // 1. it is not out of boundary
-            // 2. it has not been generated before.
-            // Because for each cell it could be generated twice.
-            if (cur.row + 1 < rows && !visited[cur.row + 1][cur.column]) {
-                minHeap.offer(new Cell(cur.row + 1, cur.column, matrix[cur.row + 1][cur.column]));
-                visited[cur.row + 1][cur.column] = true;
-            }
-            if (cur.column + 1 < columns && !visited[cur.row][cur.column + 1]) {
-                minHeap.offer(new Cell(cur.row, cur.column + 1, matrix[cur.row][cur.column + 1]));
-                visited[cur.row][cur.column + 1] = true;
-            }
+    public List<String> validParenthesis(int k) {
+        List<String> result = new ArrayList<>();
+        // the final string contains 2k characters.
+        char[] cur = new char[k * 2];
+        DFS(cur, k, k, 0, result);
+        return result;
         }
-        return minHeap.peek().value;
-    }
-
-    static class Cell {
-        int row;
-        int column;
-        int value;
-
-        Cell(int row, int column, int value) {
-            this.row = row;
-            this.column = column;
-            this.value = value;
+        
+        //left: how many ‘(’ we still have
+        //right: how many ‘)’ we still have
+        //index: the current position in cur we want to fill in with either ‘(’ or ‘)’
+        public void DFS(char[] cur, int left, int right, int index, List<String> result) {
+        //terminate condition: 
+        //when we do not have any parenthesis left
+        if (left == 0 && right == 0) {
+            result.add(new String(cur));
+            return;
         }
-    }
+        //when can we add a ‘(‘? whenever there is some ‘(‘ we can still use.
+        if (left > 0) {
+        cur[index] = '(';
+        DFS(cur, left - 1, right, index + 1, result);
+        //NOTICE: it looks like we donot do anything when back tracking to
+        //the previous level.
+        //the code is still correct because:
+        //1. we are setting the character at index and when back tracking, 
+        // what we need is just 1) remove the character at index and 2) add
+        // a different character at index.
+        //2. only when we fill in all the position in cur, we have a complete 
+        //solution.
+        //thec code itself actually already suffices the above two points and 
+        //it already does the currect removing operation when back tracked to //the previous level.
+        }
+        //when can we add a ‘)’? when there is more ‘(‘ than ‘)’ used.
+        //because each ‘)’ should be associated with a previous ‘(‘
+        if (right > left) {
+        cur[index] = ')';
+        DFS(cur, left, right - 1, index + 1, result);
+        }
+        }
+        
 }
